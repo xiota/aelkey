a861f3257c9c5c61468216af59092a1c158b99ff
xiota <github@mentalfossa.com>
2025-12-06 07:46:21 +0000

    refactor(util): move lua code to data

---
+++ data/aelkey_util.lua
@@ -0,0 +1,21 @@
+local M = ...
+
+function M.dump_events(events)
+  print(string.format("events: %d", #events))
+  for i, ev in ipairs(events) do
+    print(string.format(
+      "[%d] device=%s type=%s code=%s value=%s",
+      i, ev.device, ev.type, ev.code, ev.value
+    ))
+  end
+end
+
+function M.dump_raw(ev)
+  local data = ev.report
+  local len = #data
+  io.write(string.format("hidraw report (%d bytes):", len))
+  for i = 1, len do
+    io.write(string.format(" %02X", string.byte(data, i)))
+  end
+  io.write("\n")
+end
--- source/aelkey_util.cc
+++ source/aelkey_util.cc
@@ -8,6 +8,7 @@
 #include <lua.hpp>
 #include <time.h>
 
+#include "lua_scripts.h"
 #include "luacompat.h"
 
 // Compute one CRC32 entry
@@ -83,30 +84,8 @@ extern "C" int luaopen_aelkey_util(lua_State *L) {
 
   luaL_newlib(L, funcs);
 
-  const char *custom_code = R"(
-local M = ...
-function M.dump_events(events)
-  print(string.format("events: %d", #events))
-  for i, ev in ipairs(events) do
-    print(string.format(
-      "[%d] device=%s type=%s code=%s value=%s",
-      i, ev.device, ev.type, ev.code, ev.value
-    ))
-  end
-end
-
-function M.dump_raw(ev)
-  local data = ev.report
-  local len = #data
-  io.write(string.format("hidraw report (%d bytes):", len))
-  for i = 1, len do
-    io.write(string.format(" %02X", string.byte(data, i)))
-  end
-  io.write("\n")
-end
-)";
-
-  if (luaL_loadstring(L, custom_code) == 0) {
+  // lua scripts
+  if (luaL_loadstring(L, aelkey_util_script) == 0) {
     lua_pushvalue(L, -2);
     lua_call(L, 1, 0);
   } else {
