bcd846afcfa90a54fee5648a630ea01466e8b0c1
xiota <github@mentalfossa.com>
2025-12-10 04:06:51 +0000

    feat(luacompat): add lua_warning

--- source/luacompat.h
+++ source/luacompat.h
@@ -2,9 +2,22 @@
 
 #pragma once
 
+#include <iostream>
+
 #include <lua.hpp>
 
 #if LUA_VERSION_NUM == 501 && !defined(LUAJIT_VERSION)
 /* Compatibility definitions for plain Lua 5.1 */
 #  define luaL_newlib(L, funcs) (lua_newtable(L), luaL_register(L, nullptr, funcs))
+#endif
+
+#if LUA_VERSION_NUM < 504
+inline void lua_warning(lua_State *L, const char *msg, int tocont = 0) {
+  if (msg) {
+    std::cerr << "Lua warning: " << msg;
+    if (!tocont) {
+      std::cerr << std::endl;  // terminate warning line
+    }
+  }
+}
 #endif
