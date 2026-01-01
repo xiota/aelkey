24c0271e0c30745ebb3d76ac6d24ceab9e67d0c6
xiota <github@mentalfossa.com>
2026-01-01 22:44:04 +0000

    refactor(aelkey): migrate to sol2

--- source/aelkey.cc
+++ source/aelkey.cc
@@ -1,4 +1,4 @@
-#include <lua.hpp>
+#include <sol/sol.hpp>
 
 #include "aelkey_bit.h"
 #include "aelkey_core.h"
@@ -10,58 +10,83 @@
 #include "aelkey_usb.h"
 #include "aelkey_util.h"
 #include "lua_scripts.h"
-#include "luacompat.h"
 #include "tick_scheduler.h"
 
+namespace {
+
+struct ScriptModule {
+  const char *name;
+  const char *script;
+};
+
+struct CModule {
+  const char *name;
+  int (*open_func)(lua_State *);
+};
+
+struct TopFunc {
+  const char *name;
+  sol::function (*fn)(lua_State *);
+};
+
+// clang-format off
+constexpr ScriptModule script_modules[] = {
+  { "click", aelkey_click_script },
+};
+
+constexpr CModule c_modules[] = {
+  { "bit", luaopen_aelkey_bit },
+  { "daemon", luaopen_aelkey_daemon },
+  { "gatt", luaopen_aelkey_gatt },
+  { "hid", luaopen_aelkey_hid },
+  { "usb", luaopen_aelkey_usb },
+  { "util", luaopen_aelkey_util },
+};
+// clang-format on
+
+}  // namespace
+
 extern "C" int luaopen_aelkey(lua_State *L) {
-  // clang-format off
-  static const luaL_Reg funcs[] = {
-    {"emit", lua_emit},
-    {"start", lua_start},
-    {"stop", lua_stop},
-    {"syn_report", lua_syn_report},
-    {"tick", lua_tick},
+  sol::state_view lua(L);
 
-    {"open_device", lua_open_device},
-    {"close_device", lua_close_device},
-    {"get_device_info", lua_get_device_info},
-    {nullptr, nullptr}
-  };
-  // clang-format on
+  sol::table mod = lua.create_table();
 
-  luaL_newlib(L, funcs);
+  mod.set_function("emit", lua_emit);
+  mod.set_function("start", lua_start);
+  mod.set_function("stop", lua_stop);
+  mod.set_function("syn_report", lua_syn_report);
+  mod.set_function("tick", lua_tick);
 
-  // click submodule
-  if (luaL_loadstring(L, aelkey_click_script) == LUA_OK) {
-    lua_call(L, 0, 1);
-    lua_setfield(L, -2, "click");  // aelkey.click = module
-  } else {
-    lua_error(L);
+  mod.set_function("open_device", lua_open_device);
+  mod.set_function("close_device", lua_close_device);
+  mod.set_function("get_device_info", lua_get_device_info);
+
+  // script modules
+  for (auto &sm : script_modules) {
+    try {
+      sol::table module = lua.script(sm.script);
+      mod[sm.name] = module;
+    } catch (const sol::error &err) {
+      throw sol::error(
+          std::string("aelkey: script module '") + sm.name + "' failed: " + err.what()
+      );
+    }
   }
 
-  // bit submodule
-  luaopen_aelkey_bit(L);
-  lua_setfield(L, -2, "bit");
+  // C modules
+  for (auto &cm : c_modules) {
+    try {
+      cm.open_func(L);  // pushes table on stack
+      sol::table module = sol::stack::pop<sol::table>(L);
+      mod[cm.name] = module;
+    } catch (const sol::error &err) {
+      throw sol::error(std::string("aelkey: C module '") + cm.name + "' failed: " + err.what());
+    } catch (...) {
+      throw sol::error(
+          std::string("aelkey: C module '") + cm.name + "' failed with unknown error"
+      );
+    }
+  }
 
-  // daemon submodule
-  luaopen_aelkey_daemon(L);
-  lua_setfield(L, -2, "daemon");
-
-  // gatt submodule
-  luaopen_aelkey_gatt(L);
-  lua_setfield(L, -2, "gatt");
-
-  // hid submodule
-  luaopen_aelkey_hid(L);
-  lua_setfield(L, -2, "hid");
-
-  // usb submodule
-  luaopen_aelkey_usb(L);
-  lua_setfield(L, -2, "usb");
-
-  // util submodule
-  luaopen_aelkey_util(L);
-  lua_setfield(L, -2, "util");
-
-  return 1;
+  return sol::stack::push(L, mod);
 }
