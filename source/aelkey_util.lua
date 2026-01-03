f76be9667318a690dc9d5b7d87daed1a476866a0
xiota <github@mentalfossa.com>
2026-01-03 01:49:00 +0000

    feat(util): return string from dump functions, instead of print

--- source/aelkey_util.lua
+++ source/aelkey_util.lua
@@ -1,35 +1,48 @@
 local M = ...
 
 function M.dump_events(events)
-  print(string.format("events: %d", #events))
+  local out = {}
+  out[#out+1] = string.format("events: %d", #events)
+
   for i, ev in ipairs(events) do
-    print(string.format(
+    out[#out+1] = string.format(
       "[%d] device=%s type=%s(%s) code=%s(%s) value=%s",
       i, ev.device, ev.type_name, ev.type, ev.code_name, ev.code, ev.value
-    ))
+    )
   end
+
+  return table.concat(out, "\n")
 end
 
 function M.dump_raw(ev)
   local data = ev.data
   local len = #data
-  io.write(string.format("raw data (%d bytes):", len))
+  local out = {}
+
+  out[#out+1] = string.format("raw data (%d bytes):", len)
+
   for i = 1, len do
-    io.write(string.format(" %02X", string.byte(data, i)))
+    out[#out+1] = string.format(" %02X", string.byte(data, i))
   end
-  io.write("\n")
+
+  return table.concat(out)
 end
 
-function M.dump_table(t, indent)
-  indent = indent or "  "
-  print "{"
+local function dump_table_inner(t, indent, out)
   for k, v in pairs(t) do
     if type(v) == "table" then
-      print(indent .. tostring(k) .. " = ")
-      print_table(v, indent .. "  ")
+      out[#out+1] = string.format("%s%s = {", indent, tostring(k))
+      dump_table_inner(v, indent .. "  ", out)
+      out[#out+1] = indent .. "}"
     else
-      print(indent .. tostring(k) .. " = " .. tostring(v))
+      out[#out+1] = string.format("%s%s = %s", indent, tostring(k), tostring(v))
     end
   end
-  print "}"
+end
+
+function M.dump_table(t)
+  local out = { "{" }
+  dump_table_inner(t, "  ", out)
+  out[#out+1] = "}"
+  return table.concat(out, "\n")
 end
