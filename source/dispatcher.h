2a88c4fff70204fc9012cf3b37708406ec92b7a2
xiota <github@mentalfossa.com>
2026-01-24 01:47:14 +0000

    refactor(dispatcher): centralize epoll fd/payload management

--- source/dispatcher.h
+++ source/dispatcher.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <unordered_map>
 #include <vector>
+
+#include <sys/epoll.h>
+
+#include "aelkey_state.h"
 
 template <typename T>
 struct DispatcherRegistry;
@@ -16,13 +21,63 @@ struct EpollPayload {
 // Polymorphic base class for all dispatchers
 class DispatcherBase {
  public:
-  virtual ~DispatcherBase() = default;
+  virtual ~DispatcherBase() {
+    cleanup_fds();
+  }
 
   virtual void init() {}
   virtual void handle_event(EpollPayload *payload, uint32_t events) {};
 
+  EpollPayload *get_payload(int fd) const {
+    auto it = pollfds_.find(fd);
+    return (it != pollfds_.end()) ? it->second : nullptr;
+  }
+
+  virtual void register_fd(int fd, uint32_t events) {
+    auto &state = AelkeyState::instance();
+
+    auto *payload = new EpollPayload{ this, fd };
+
+    struct epoll_event ev{};
+    ev.events = events;
+    ev.data.ptr = payload;
+
+    if (epoll_ctl(state.epfd, EPOLL_CTL_ADD, fd, &ev) < 0) {
+      perror("epoll_ctl ADD");
+      delete payload;
+      return;
+    }
+
+    pollfds_[fd] = payload;
+  }
+
+  virtual void unregister_fd(int fd) {
+    auto &state = AelkeyState::instance();
+
+    auto it = pollfds_.find(fd);
+    if (it == pollfds_.end()) {
+      return;
+    }
+
+    epoll_ctl(state.epfd, EPOLL_CTL_DEL, fd, nullptr);
+
+    delete it->second;
+    pollfds_.erase(it);
+  }
+
+  virtual void cleanup_fds() {
+    auto &state = AelkeyState::instance();
+
+    for (auto &[fd, payload] : pollfds_) {
+      epoll_ctl(state.epfd, EPOLL_CTL_DEL, fd, nullptr);
+      delete payload;
+    }
+    pollfds_.clear();
+  }
+
  protected:
   DispatcherBase() = default;
+  std::unordered_map<int, EpollPayload *> pollfds_;
 };
 
 // CRTP dispatcher class
